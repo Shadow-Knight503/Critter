91382977033ec0d919e3447fda29a122d4aa7233
Shadow-Knight503 <90497821+Shadow-Knight503@users.noreply.github.com>
2024-01-30 01:59:22 +0530

    Project Update

--- src/components/Proj.tsx
+++ src/components/Proj.tsx
@@ -8,36 +8,38 @@ function Proj() {
     const Prm: any = useParams()
 
     const [Proj, setProj] = useState({
-        Name: "", Descrp: "", Img: "", Numb: "", Budget: "", ID: "", Team: {},
+        Name: "", Descrp: "", Img: "", Numb: "", Budget: "", ID: "", Status: "", 
+        Team: {}, Load: false
     })
-    // const commonProps = {myProp1: 'prop1',myProp2: 'prop2'};
-    // let Users: any = {}
 
     useEffect(() => {
         const getProj = async () => {
             const ProjRef = doc(Db, 'Crit_Proj', Prm.pid)
             const ProjSnap = await getDoc(ProjRef)
-            const Proj: any = {...ProjSnap.data(), ID: ProjSnap.id} 
+            const Proj: any = {...ProjSnap.data(), ID: ProjSnap.id, Load: true} 
             setProj(Proj)
         }
         getProj()
-      }, [])
+    }, [])
 
     return (
         <>
             <br />
             <div className='container flex gap-2'>
                 <div className='grow'>
-                    <div className='mt-[2.5vh] max-w-[60vw] bg-Thir rounded'>
-                        <img className='rounded min-w-1/4 min-h-1/4 p-[1.7ch]' src={Proj.Img} />
-                    </div>
-                    <div className='m-[2.5vh] max-w-[40em] text-slate-200  font-Comf'>
-                        <p className='text-4xl font-Oswd text-Text'>{Proj.Name}</p>
-                        <p className='text-2xl'>Rs {Proj.Budget}</p>
-                        <p className='text-xl'>{Proj.Descrp}</p>
+                    <div className='flex mt-[2.5vh] max-w-[60vw] bg-Thir rounded'>
+                        <img className='object-cover rounded min-w-1/4 min-h-1/4 p-[1.7ch]' src={Proj.Img} />
+                        <div className='m-[2.5vh] max-w-[40em] text-slate-200  font-Comf'>
+                            <p className='text-4xl font-Oswd text-Text'>{Proj.Name}</p>
+                            <p className='text-2xl'>Rs {Proj.Budget}</p>
+                            <p className='text-xl'>{Proj.Descrp}</p>
+                        </div>
                     </div>
                 </div>
-                <Team Team={Proj.Team} />
+                {Proj.Load ? 
+                    <Team Team={Proj.Team} Descrp={Proj.Descrp} 
+                        Status={Proj.Status}/>: false
+                }                
             </div>
         </>
     )
--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,27 +1,53 @@
-import { useEffect } from "react"
+import { doc, getDoc } from "firebase/firestore"
+import { useEffect, useState } from "react"
+import { Db } from "../Config"
 
 function Team(props: any) {
-    // const [Users, setUsers] = useState([{
-    //     Name: "", Pfp: "", ID: ""
-    // }])
-    const { Team }: any = props
+    const [Users, setUsers] = useState([{
+        Name: "", Pfp: "", ID: "", Role: ""
+    }])
+    const { Team, Descrp, Status }: any = props
 
     useEffect(() => {
-        async () => {
-            console.log(Team)
+        const getUsers = async () => {
+            const TeamDets: any = []
+            for(let el in Team) {
+                const DetsSnap: any = await getDoc(doc(Db, Team[el][0]))
+                const Dets: any = {Name: DetsSnap.data().Fname + " " + 
+                    DetsSnap.data().Lname, ID: DetsSnap.id, Pfp: DetsSnap.data().Pfp, 
+                    Role: Team[el][1]}
+                TeamDets.push(Dets)
+            }    
+            setUsers(TeamDets)
         }
-        // if(Team) {
-        //     await 
-        // } else {
-        //     console.log(null)
-        // }
+        getUsers()
     }, [])
 
     return (
         <>
-            <div className=' bg-slate-800 h-[100vh] 
-                min-w-[25vw]'>
-                <p className="text-xl text-white">{}</p>
+            <div className='relative bg-slate-900 h-[90vh] p-3 overflow-y-auto 
+              text-slate-200 font-Comf min-w-[25vw] max-w-[30vw]'>
+                <p className="text-Text font-Oswd text-2xl">Team: </p>
+                <div className="flex gap-2 mb-3">
+                    {Users.map((User) => (
+                        <div className="" key={User.ID}>
+                            <img className="rounded-full w-[3rem] h-[3rem] object-cover" src={User.Pfp}></img>
+                        </div>
+                    ))}
+                </div>
+                {Users.map((User) => (
+                    <p className="truncate mb-1 text-md font-Comf"
+                        key={User.ID}>{User.Role}: {User.Name}</p> 
+                ))}                
+                <br />
+                <p className="mb-1 font-Oswd bold text-Text text-xl">Work Description:</p>
+                <p className="text-wrap">{Descrp}</p>
+                <div className="absolute bottom-5 text-2xl">Status:  
+                    {Status === 'Completed'? 
+                        <span className="text-green-400"> {Status}</span>: 
+                        <span className="text-orange-500"> {Status}</span> 
+                    }
+                </div>
             </div>        
         </>
     )
